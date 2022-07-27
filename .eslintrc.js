f768c22e27d705e2b2855bc58d143268e2872426
Ighor Alves <ighoralc@gmail.com>
2022-07-27 18:30:00 -0300

    inicio

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "import/first": "off",
+  },
+  "globals": {
+    "document": true,
+    "window": true,
+    "jQuery": true
+  }
+};
